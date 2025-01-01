59a63ffafb47c3feb4b52a158dae93ffd7613423
yousseftechdev <youssefmostafa200895@gmail.com>
2025-01-01 18:28:51 +0200

    modded README

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c24609cb48833365"; }
+/******/ 		__webpack_require__.h = function() { return "a6897b58b56a7b11"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
