85e6322baac25f49961615695eb9e6c1245ccc39
yousseftechdev <youssefmostafa200895@gmail.com>
2025-01-01 18:05:37 +0200

    first commit

---
+++ .next/static/webpack/pages/index.d5bb2530164938e0.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/index",{
+
+/***/ "./config.json":
+/*!*********************!*\
+  !*** ./config.json ***!
+  \*********************/
+/***/ (function(module) {
+
+module.exports = JSON.parse('{"readmeUrl":"https://raw.githubusercontent.com/yousseftechdev/yousseftechdev/master/README.md","title":"YTerm - Personal Portfolio","name":"Youssef Mostafa","ascii":"liveterm","social":{"github":"yousseftechdev","linkedin":"linkedin"},"email":"youssefmostafa200895@email.com","ps1_hostname":"yterm","ps1_username":"visitor","repo":"https://github.com/yousseftechdev/YTerm","portfolio_url":"https://upload.wikimedia.org/wikipedia/commons/c/cc/Resume.pdf","donate_urls":{"paypal":"https://paypal.me/cveinnt","patreon":"https://patreon.com/cveinnt"},"colors":{"light":{"background":"#FBF1C9","foreground":"#3C3836","yellow":"#D79921","green":"#98971A","gray":"#7C6F64","blue":"#458588","red":"#CA2124"},"dark":{"background":"#2E3440","foreground":"#E5E9F0","yellow":"#5E81AC","green":"#A3BE8C","gray":"#88C0D0","blue":"#EBCB8B","red":"#BF616A"}}}');
+
+/***/ })
+
+});
